db0dd225417605ff0447326773aae8a8b0ad0278
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-22 20:06:46 -0700

    Books greater than 0 display functionality added

--- src/Pages/AllBooks.jsx
+++ src/Pages/AllBooks.jsx
@@ -23,12 +23,25 @@ const AllBooks = () => {
     navigate(`/updateBook/${id}`);
   };
 
+  const showAvailableBooks = () => {
+    const filteredData = books.filter((book) => book.quantity > 0);
+    setBooks(filteredData);
+  };
+
   return (
     <div>
       <Helmet>
         <title>All Books</title>
       </Helmet>
       <div className="max-w-6xl mx-auto my-2 px-2">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-2 px-4">
+          <button onClick={showAvailableBooks} className="btn">
+            Available Books
+          </button>
+          <button onClick={getAllBooks} className="btn">
+            All Books
+          </button>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-5 ">
           {books.map((eachBook) => (
             <div className="card bg-base-100 shadow-xl">
@@ -39,11 +52,7 @@ const AllBooks = () => {
                 <h2 className="card-title">{eachBook.title}</h2>
                 <p>{eachBook.authorName}</p>
                 <p>{eachBook.category}</p>
-                <ReactStars
-                  count={5}
-                  value={eachBook.rating}
-                  size={24}
-                />
+                <ReactStars count={5} value={eachBook.rating} size={24} />
                 <div className="card-actions justify-end">
                   <button
                     onClick={() => {
