50edeb34bab9e4dcb3838cc23ab2d8cd0e130893
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-22 20:48:04 -0700

    Toggle view feature implemented

--- src/Pages/AllBooks.jsx
+++ src/Pages/AllBooks.jsx
@@ -6,6 +6,8 @@ import ReactStars from "react-rating-stars-component";
 
 const AllBooks = () => {
   const [books, setBooks] = useState([]);
+  const [isCardView, setIsCardView] = useState(true);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -51,40 +53,98 @@ const AllBooks = () => {
               tabIndex={0}
               className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
             >
-              <li>
+              <li
+                onClick={() => {
+                  setIsCardView(true);
+                }}
+              >
                 <a>Card View</a>
               </li>
-              <li>
+              <li
+                onClick={() => {
+                  setIsCardView(false);
+                }}
+              >
                 <a>Table View</a>
               </li>
             </ul>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-5 ">
-          {books.map((eachBook) => (
-            <div className="card bg-base-100 shadow-xl">
-              <figure>
-                <img src={eachBook.coverImage} alt="Cover Image" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">{eachBook.title}</h2>
-                <p>{eachBook.authorName}</p>
-                <p>{eachBook.category}</p>
-                <ReactStars count={5} value={eachBook.rating} size={24} />
-                <div className="card-actions justify-end">
-                  <button
-                    onClick={() => {
-                      handleUpdate(eachBook._id);
-                    }}
-                    className="btn"
-                  >
-                    Update
-                  </button>
+        {isCardView ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-5 ">
+            {books.map((eachBook) => (
+              <div className="card bg-base-100 shadow-xl">
+                <figure>
+                  <img src={eachBook.coverImage} alt="Cover Image" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{eachBook.title}</h2>
+                  <p>{eachBook.authorName}</p>
+                  <p>{eachBook.category}</p>
+                  <ReactStars count={5} value={eachBook.rating} size={24} />
+                  <div className="card-actions justify-end">
+                    <button
+                      onClick={() => {
+                        handleUpdate(eachBook._id);
+                      }}
+                      className="btn"
+                    >
+                      Update
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Book</th>
+                  <th>Category</th>
+                  <th>Rating</th>
+                  <th>Update</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {books.map((eachBook) => (
+                  <tr>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-12 w-12">
+                            <img src={eachBook.coverImage} alt="Book Cover" />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">{eachBook.title}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>{eachBook.category}</td>
+                    <td>
+                      <ReactStars count={5} value={eachBook.rating} size={24} />
+                    </td>
+                    <th>
+                      <button
+                        onClick={() => {
+                          handleUpdate(eachBook._id);
+                        }}
+                        className="btn"
+                      >
+                        Update
+                      </button>
+                    </th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     </div>
   );
