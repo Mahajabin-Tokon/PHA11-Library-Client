5d53c3f460c4492854052579b0322b0c836cc1c8
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-24 20:19:00 -0700

    Used framer motion for categories

--- src/Components/BookCategories.jsx
+++ src/Components/BookCategories.jsx
@@ -1,29 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "motion/react";
 
 const BookCategories = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-5 pb-20">
-      <div className="card bg-base-100 shadow-xl">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 pt-5 pb-20">
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        className="card bg-base-100 shadow-xl"
+      >
         <Link to="/history" className="btn card-body">
           <h2 className="card-title">History</h2>
         </Link>
-      </div>
-      <div className="card bg-base-100 shadow-xl">
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        className="card bg-base-100 shadow-xl"
+      >
         <Link to="/romance" className="btn card-body">
           <h2 className="card-title">Romance</h2>
         </Link>
-      </div>
-      <div className="card bg-base-100 shadow-xl">
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        className="card bg-base-100 shadow-xl"
+      >
         <Link to="/business" className="btn card-body">
           <h2 className="card-title">Business</h2>
         </Link>
-      </div>
-      <div className="card bg-base-100 shadow-xl">
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        className="card bg-base-100 shadow-xl"
+      >
         <Link to="/fantasy" className="btn card-body">
           <h2 className="card-title">Fantasy</h2>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
